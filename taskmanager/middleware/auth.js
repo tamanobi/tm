634e3016221fa3da86df1b1896ba0f11aa07dcd9
tamanobi <tamanobi@gmail.com>
2021-04-20 01:48:55 +0900

    chore: remove unused vars

--- taskmanager/middleware/auth.js
+++ taskmanager/middleware/auth.js
@@ -5,9 +5,7 @@ export default function ({ store }) {
   const firebase = getters['firebase/getFirebase']
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-      console.log('gotUser')
       dispatch('auth/gotUser', user)
     }
   })
-  console.log('gotUser')
 }
